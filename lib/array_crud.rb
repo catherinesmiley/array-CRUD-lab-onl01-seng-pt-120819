ff0c401d4bdc76a4d43dc044e80888ac072122d0
Catherine Smiley <catherinesmiley@gmail.com>
2019-12-27 03:50:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,69 @@
+# def create_an_empty_array
+#   create_an_empty_array = []
+# end
+
+# def create_an_array
+#   create_an_array = ["Betty", "Archie", "Jughead", "Veronica"]
+# end
+
+# def add_element_to_end_of_array(array, element)
+#   array << "arrays!"
+# end
+
+# def add_element_to_start_of_array(array, element)
+#   array.unshift("wow")
+# end
+
+# def remove_element_from_end_of_array(array)
+#   array.pop
+# end
+
+# def remove_element_from_start_of_array(array)
+#   array.shift
+# end
+
+# def retrieve_element_from_index(array, index_number)
+#   array[2]
+# end
+
+# def retrieve_first_element_from_array(array)
+#   array[0]
+# end
+
+# def retrieve_last_element_from_array(array)
+#   array[-1]
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def create_an_empty_array
-  create_an_empty_array = []
+  []
 end
 
 def create_an_array
-  create_an_array = ["Betty", "Archie", "Jughead", "Veronica"]
+  ["Simon", "Klara", "Daniel", "Varya"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "arrays!"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +75,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[2]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
